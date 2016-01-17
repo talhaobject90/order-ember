3b577826aa735f383d128ecb8d954756fdd4bd58
Mohammed Talha <talhaqqq@gmail.com>
2016-01-17 17:20:29 +0530

    After fixing index issues

--- config/environment.js
+++ config/environment.js
@@ -53,9 +53,9 @@ module.exports = function(environment) {
     ENV.APP.host =  'http://localhost:3000';
     // ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+     ENV.APP.LOG_TRANSITIONS = true;
+     ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
+    ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
 
   if (environment === 'test') {
